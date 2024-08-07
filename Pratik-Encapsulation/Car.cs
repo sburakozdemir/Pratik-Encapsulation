12b1126c1291e471ec9a9a4ccea8cec341b84426
sburakozdemir <sburakozdemir00@gmail.com>
2024-08-07 12:07:48 +0300

    Add project files.

---
+++ Pratik-Encapsulation/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Pratik_Encapsulation
+{
+    internal class Car
+    {
+        private string _brand;
+        private string _model;
+        private string _color;
+        private int _doorCount;
+
+        public Car(string brand, string model, string color, int doorCount)
+        {
+            _brand = brand;
+            _model = model;
+            _color = color;
+            DoorCount = doorCount; // Burada DoorCount özelliğini kullanıyoruz
+        }
+
+        public string Brand { get { return _brand; } }
+        public string Model { get { return _model; } }
+        public string Color { get { return _color; } }
+
+        public int DoorCount
+        {
+            get { return _doorCount; }
+            set
+            {
+                if (value == 2 || value == 4)
+                    _doorCount = value;
+                else
+                {
+                    Console.WriteLine("Hatalı giriş: Kapı sayısı 2 veya 4 olmalıdır.");
+                    _doorCount = -1; // Hatalı giriş durumunda -1 atıyoruz
+                }
+            }
+        }
+
+        public void InfoCar()
+        {
+            Console.WriteLine($"Markası {_brand}, modeli {_model}, rengi {_color}, kapı sayısı {_doorCount.ToString()} olan bir araba oluşturdunuz");
+        }
+    }
+}
---
+++ Pratik-Encapsulation/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using Pratik_Encapsulation;
+using System.Reflection;
+
+Car car1 = new Car("Tesla", "Model Y", "Dark Silver", 4);
+car1.InfoCar();
+
+Car car2 = new Car("Toyota", "Corolla", "White", 3);
+car2.InfoCar();
